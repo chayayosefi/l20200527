2524df8e6c743613fc74f26b8144241143bacafa
dhstudents <dhgithub@gmail.com>
2020-06-01 12:04:36 +0300

    lesson in class

---
+++ arrow-function.js
@@ -0,0 +1,25 @@
+lala()
+
+function lala() {}
+
+let lalala = function (a,b) { return a + b }
+
+lalala()
+
+let lili01 = () => { }
+let lili02 = (a, b) => a + b // return a + b
+let lili03 = a => a + 10 // return a + b
+let lili04 = () => new Date() // return a + b
+let lili05 = (a, b) => {
+    a += 10;
+  //  a /= b;
+    return a /= b;
+}
+
+console.log(lili05(5, 2))
+
+
+
+
+
+
